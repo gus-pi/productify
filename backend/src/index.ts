1ddd175f91d2b8f6db0bc9aab9b052edcecc440e
PieMenard <pinedo.m.gustavo@gmail.com>
2026-02-03 15:06:19 -0400

    chore: update package.json scripts and add production static file serving; change default theme in ThemeSelector

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,5 +1,6 @@
 import express from 'express';
 import cors from 'cors';
+import path from 'path';
 
 import { ENV } from './config/env';
 import { clerkMiddleware } from '@clerk/express';
@@ -14,7 +15,7 @@ app.use(express.json()); //parses JSON request bodies
 app.use(express.urlencoded({ extended: true })); //parses json data from forms
 app.use(cors({ origin: ENV.FRONTEND_URL, credentials: true }));
 
-app.get('/', (req, res) => {
+app.get('/api/health', (req, res) => {
     res.json({
         message: 'Welcome to Productify API - Powered by PostgreSQL, Drizzle ORM & Clerk Auth',
         endpoints: {
@@ -28,5 +29,17 @@ app.get('/', (req, res) => {
 app.use('/api/users', userRoutes);
 app.use('/api/products', productRoutes);
 app.use('/api/comments', commentRoutes);
+
+if (ENV.NODE_ENV === 'production') {
+    const __dirname = path.resolve();
+
+    //serve statis files from frontend/dist
+    app.use(express.static(path.join(__dirname, '../frontend/dist')));
+
+    //handle SPA routing - send all non-api routes to index.html - react app
+    app.get('/{*any}', (req, res) => {
+        res.sendFile(path.join(__dirname, '../frontend/dist/index.html'));
+    });
+}
 
 app.listen(ENV.PORT, () => console.log('Server is up and running on PORT:', ENV.PORT));
